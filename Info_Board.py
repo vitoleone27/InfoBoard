aa0034679b1af1126a8c9dfef2433b606f46bd89
Vito <vitoleone27@gmail.com>
2022-05-10 14:02:51 -0400

    Weather button scrapes website, sports button goes to espn

--- Info_Board.py
+++ Info_Board.py
@@ -3,6 +3,8 @@
 from tkinter import *
 import tkinter.messagebox
 import webbrowser
+from bs4 import BeautifulSoup
+import requests
 
 window = Tk()
 window.title("Info Board")
@@ -10,7 +12,31 @@ width, height = window.winfo_screenwidth(), window.winfo_screenheight()
 window.geometry('%dx%d+0+0' % (width, height))
 
 def openWeather():
-    webbrowser.open('https://weather.com/weather/today/l/0bf9c2fb36e8620ba2743f3959a103d36e9ddab6fe86c9b47940cc129e36af97')
+    weatherWindow = Toplevel(window)
+    weatherWindow.title("Weather")
+    width, height = weatherWindow.winfo_screenwidth(), weatherWindow.winfo_screenheight()
+    weatherWindow.geometry('%dx%d+0+0' % (width, height))
+    canvas = Canvas(weatherWindow, width=width, height=height, bg="DarkBlue")
+
+    contents = requests.get('http://forecast.weather.gov/MapClick.php?lat=40.6222&lon=-75.3932')
+    soup = BeautifulSoup(contents.text, 'html.parser')
+
+    locations = soup.find('div', class_='panel panel-default').find_next_sibling('div')
+    locations = locations.find('h2', class_='panel-title')
+    canvas.create_text(850, 100, text=locations.string, fill="white", font=("Helvetica 15 bold"))
+
+    forecasts = soup.find_all('img', class_='forecast-icon')
+    startY = 200
+    for forecast in forecasts:
+        canvas.create_text(850, startY, text=forecast['alt'], fill="white", font=("Helvetica 15 bold"))
+        startY += 50
+
+    createButton(weatherWindow, "Close", 850, 700)
+
+    canvas.pack()
+
+def openSports():
+    webbrowser.open("https://www.espn.com/")
 
 def createAllButtons():
     createButton(window, "Reminders", 0, 0)
@@ -20,6 +46,7 @@ def createAllButtons():
     createButton(window, "Close", 850, 700)
 
 def createButton(window, name, x, y):
+    buttonText = StringVar()
     if name == "Reminders":
         button = Button(window, text=name, command=openWeather, height = 25, width = 50)
     elif name == "Weather":
@@ -27,7 +54,7 @@ def createButton(window, name, x, y):
     elif name == "Jokes":
         button = Button(window, text=name, command=openWeather, height = 25, width = 50)
     elif name == "Sports":
-        button = Button(window, text=name, command=openWeather, height = 25, width = 50)
+        button = Button(window, text=name, command=openSports, height = 25, width = 50)
     else:
         button = Button(window, text=name, command=window.destroy, width = 25)
     button.place(x=x, y=y)
