de90b99e75d8ae3ce8649a4fb591309af0b72595
Vito <vitoleone27@gmail.com>
2022-05-11 19:37:36 -0400

    Pushed for debugging cleanString() on latop

--- Info_Board.py
+++ Info_Board.py
@@ -5,11 +5,23 @@ import tkinter.messagebox
 import webbrowser
 from bs4 import BeautifulSoup
 import requests
+from sys import getsizeof
 
 window = Tk()
 window.title("Info Board")
 width, height = window.winfo_screenwidth(), window.winfo_screenheight()
 window.geometry('%dx%d+0+0' % (width, height))
+
+def cleanString(description):
+    i = 1
+    try:
+        for i in description.len():
+            if description[i].isupper():
+                description[:i] + " " + description[i:]
+            i+=1
+        return description
+    except:
+        return description
 
 def openWeather():
     weatherWindow = Toplevel(window)
@@ -18,20 +30,32 @@ def openWeather():
     weatherWindow.geometry('%dx%d+0+0' % (width, height))
     canvas = Canvas(weatherWindow, width=width, height=height, bg="DarkBlue")
 
-    contents = requests.get('http://forecast.weather.gov/MapClick.php?lat=40.6222&lon=-75.3932')
+
+    contents = requests.get('https://forecast.weather.gov/MapClick.php?lat=40.1944&lon=-75.2429')
     soup = BeautifulSoup(contents.text, 'html.parser')
 
-    locations = soup.find('div', class_='panel panel-default').find_next_sibling('div')
-    locations = locations.find('h2', class_='panel-title')
-    canvas.create_text(850, 100, text=locations.string, fill="white", font=("Helvetica 15 bold"))
+    location = soup.find('div', class_='panel panel-default').find_next_sibling('div')
+    location = location.find('h2', class_='panel-title')
 
-    forecasts = soup.find_all('img', class_='forecast-icon')
+    canvas.create_text(width/2, 50, text=location.string.strip(), fill="white", font=("Helvetica 30 bold"))
+
+    days = soup.find_all('p', class_='period-name')
+    descriptions = soup.find_all('p', class_='short-desc')
+    tempLows = soup.find_all('p', class_="temp temp-low")
+    ##forecasts = soup.find_all('img', class_='forecast-icon')
+
     startY = 200
-    for forecast in forecasts:
-        canvas.create_text(850, startY, text=forecast['alt'], fill="white", font=("Helvetica 15 bold"))
+    ##for forecast in forecasts:
+       ## canvas.create_text(width/2-25, startY, text=forecast['alt'], fill="white", font=("Helvetica 15"))
+      ##  startY += 50
+    i = 0
+    for day in days:
+        dayName = cleanString(day.get_text())
+        canvas.create_text(width/2-25, startY, text=dayName + "\n " + descriptions[i].get_text(), fill="white", font=("Helvetica 15"))
+        i += 1
         startY += 50
 
-    createButton(weatherWindow, "Close", 850, 700)
+    createButton(weatherWindow, "Close")
 
     canvas.pack()
 
@@ -39,25 +63,30 @@ def openSports():
     webbrowser.open("https://www.espn.com/")
 
 def createAllButtons():
-    createButton(window, "Reminders", 0, 0)
-    createButton(window, "Weather", 0, 550)
-    createButton(window, "Jokes", width - 450, 0)
-    createButton(window, "Sports", width - 450, 550)
-    createButton(window, "Close", 850, 700)
+    createButton(window, "Reminders")
+    createButton(window, "Weather")
+    createButton(window, "Jokes")
+    createButton(window, "Sports")
+    createButton(window, "Close")
 
-def createButton(window, name, x, y):
+def createButton(window, name):
     buttonText = StringVar()
     if name == "Reminders":
         button = Button(window, text=name, command=openWeather, height = 25, width = 50)
+        button.place(x=0, y=0)
     elif name == "Weather":
         button = Button(window, text=name, command=openWeather, height = 25, width = 50)
+        button.place(x=0, y=550)
     elif name == "Jokes":
         button = Button(window, text=name, command=openWeather, height = 25, width = 50)
+        button.place(x=width - 480, y=0)
     elif name == "Sports":
         button = Button(window, text=name, command=openSports, height = 25, width = 50)
+        button.place(x=width - 480, y=550)
     else:
-        button = Button(window, text=name, command=window.destroy, width = 25)
-    button.place(x=x, y=y)
+        button = Button(window, text=name, command=window.destroy, width = 25, font=("Helvetica 15"))
+        button.place(relx=.5, rely=.8, anchor=CENTER)
+
 
 createAllButtons()
 
